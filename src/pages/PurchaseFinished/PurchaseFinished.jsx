836f9bd119c8e17329bf8dfac5a3e47628cef438
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-10-09 23:00:16 +1100

    changes bg image complete purchase

--- src/pages/PurchaseFinished/PurchaseFinished.jsx
+++ src/pages/PurchaseFinished/PurchaseFinished.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
-import image from "../../assets/images/completed.png";
+import logo from "../../assets/images/completed.png";
 import styles from "./PurchaseFinished.module.scss"; 
 
 const PurchaseFinished = () => {
   return (
     <main className={styles["main-section"]}>
-      <img className={styles["main-section__img"]} src={image} alt="thank you for supporting a small business" />
+      <img className={styles["main-section__img"]} src={logo} alt="thank you for supporting a small business" />
       <Link className={styles["main-section__back"]} to="/">back to main page</Link>
     </main>
   );
